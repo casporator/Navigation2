cd668b79641b5492b9356eacc9b624530f97c7e0
Oleg Popov <108875963+casporator@users.noreply.github.com>
2022-11-12 20:35:10 +0100

    Error RESULT done

--- Navigation/Models/Checker.swift
+++ Navigation/Models/Checker.swift
@@ -15,22 +15,60 @@ protocol LoginViewControllerDelegate{
 class Checker {
     static let shared = Checker()
     
-    private let login = "1"
-    private let password = "1"
-    
-    func checkLogin(login: String, password: String) -> Bool {
-        self.login == login && self.password == password ? true : false
-    }
+    private var EnterLogin: String
+    private var EnterPassword: String
     
     private init(){
         
+        self.EnterLogin = "1"
+        self.EnterPassword = "1"
+        
+  }
+    
+    func checkLogin(login: String, password: String) -> Result<Bool, loginError>  {
+        if login == EnterLogin && password == EnterPassword {
+            return .success(true)
+        }
+        if login == "" && password != "" {
+            return .failure(.loginEmpty)
+        }
+        if password == "" && login != "" {
+            return.failure(.passwordEmpty)
+        }
+        if login == "" && password == "" {
+            return .failure(.empty)
+        }
+        else {
+            return .failure(.incorrect)
+        }
     }
+    
+  
 }
 
 struct LoginInspector: LoginViewControllerDelegate {
     func checkLogin(controller: LoginViewController,
                     login: String, password: String) -> Bool {
-        return Checker.shared.checkLogin(login: login, password: password)
+        switch Checker.shared.checkLogin(login: login, password: password) {
+        case .success(true):
+            print("Логин и пароль - верны")
+            return true
+        case .failure(loginError.loginEmpty):
+            print("Error: Поле логина не заполненно")
+            return false
+        case .failure(loginError.passwordEmpty):
+            print("Error: Поле пароля не заполнеено")
+            return false
+        case .failure(loginError.empty):
+            print("Error: Оба заполняемых поля пустые")
+            return false
+        case .failure(loginError.incorrect):
+            print("Error: Не правильный логин или пароль")
+            return false
+        case .success(false):
+            print("Error: Не известная ошибка")
+            return false
+        }
         
     }
 }
