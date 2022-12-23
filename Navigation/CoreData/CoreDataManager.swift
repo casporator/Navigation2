5e2f15e28bd4150dbb0696b1a7aa59f64b6fcfc5
Oleg Popov <108875963+casporator@users.noreply.github.com>
2022-12-23 17:38:52 +0100

    create PostDataManager

---
+++ Navigation/CoreData/CoreDataManager.swift
@@ -0,0 +1,67 @@
+//
+//  CoreDataManager.swift
+//  Navigation
+//
+//  Created by Oleg Popov on 23.12.2022.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    static let defaultManager = CoreDataManager()
+    var posts: [LikesPostModel] = []
+    
+    init() {}
+    
+    lazy var persistentConteiner: NSPersistentContainer = {
+        
+        let conteiner = NSPersistentContainer(name: "coreData")
+        conteiner.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+                
+            }
+        })
+        return conteiner
+    } ()
+    
+    func saveContext() {
+        let context = persistentConteiner.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            }catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    func addPost(author: String, descriptionText: String, image: String, likes: Int64, views: Int64) {
+        let newPost = LikesPostModel(context: persistentConteiner.viewContext)
+        newPost.author = author
+        newPost.descriptionText = descriptionText
+        newPost.likes = likes
+        newPost.views = views
+        newPost.image = image
+     
+        saveContext()
+        reloadPosts()
+    }
+    
+    func reloadPosts() {
+        let request = LikesPostModel.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "author", ascending: true)]
+        do{
+            let fetchedPost = try persistentConteiner.viewContext.fetch(request)
+            posts = fetchedPost
+        } catch {
+            print("error")
+            posts = []
+        }
+    }
+    
+}
